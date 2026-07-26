920d776dc06639ebdb6f8c9c70538c9bdca2e048
Ideegourmande <vkloetzli@bluewin.ch>
2026-07-26 15:48:31 +0200

    Implement automatic cart total calculation
    Added a function to calculate the total price of items in the cart.

--- js/commande.js
+++ js/commande.js
@@ -1,1 +1,31 @@
+// ==================================
+// IDÉE GOURMANDE
+// Calcul automatique du panier
+// ==================================
 
+
+function calculerTotal() {
+
+
+    let foieFigues = document.getElementById("foieFigues").value * 35;
+
+    let foiePiment = document.getElementById("foiePiment").value * 35;
+
+    let magret = document.getElementById("magret").value * 25;
+
+    let saumon = document.getElementById("saumon").value * 8;
+
+
+
+    let total = 
+        foieFigues +
+        foiePiment +
+        magret +
+        saumon;
+
+
+
+    document.getElementById("total").innerHTML =
+        "Total : " + total.toFixed(2) + " CHF";
+
+}
