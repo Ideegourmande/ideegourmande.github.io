5b19533942da2124d6bff307e0975e880f29ed22
Ideegourmande <vkloetzli@bluewin.ch>
2026-07-27 11:37:28 +0200

    Add admin login function with password validation
    Implement admin login functionality with password check.

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,1 +1,47 @@
+// ==================================
+// Connexion administration
+// ==================================
 
+
+function connexionAdmin(){
+
+
+const motdepasse =
+document.getElementById("motdepasse").value;
+
+
+
+// À modifier par votre propre mot de passe
+
+const motdepasseAdmin =
+"idee2026";
+
+
+
+if(motdepasse === motdepasseAdmin){
+
+
+localStorage.setItem(
+"adminConnecte",
+"oui"
+);
+
+
+
+window.location.href =
+"admin.html";
+
+
+}
+
+else{
+
+
+document.getElementById("message").innerHTML =
+"❌ Mot de passe incorrect";
+
+
+}
+
+
+}
