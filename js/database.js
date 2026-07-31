64f8aade6506d97cbb1c837b059ca1b05e6f017f
Ideegourmande <vkloetzli@bluewin.ch>
2026-07-31 17:41:41 +0200

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,83 +1,202 @@
- // ======================================
+// ======================================
 // IDEE GOURMANDE
 // Base de données centrale
-// Version 2.0.1
+// Version 2.1.0
 // ======================================
 
 
-let db = JSON.parse(localStorage.getItem("ideeGourmandeDB"));
+let db = JSON.parse(
+    localStorage.getItem("ideeGourmandeDB")
+);
+
+
+
+const emplacementsDefaut = [
+
+    "Congélateur du réduit",
+
+    "Congélateur bahut",
+
+    "Congélateur GI",
+
+    "Chambre froide",
+
+    "Cave",
+
+    "Réserve sèche"
+
+];
 
 
 
 const structureDB = {
 
+
     commandes: [],
+
 
     articles: [],
 
-    emplacements: [
 
-        "Congélateur du réduit",
+    emplacements: emplacementsDefaut,
 
-        "Congélateur bahut",
-
-        "Congélateur GI",
-
-        "Chambre froide",
-
-        "Cave",
-
-        "Réserve sèche"
-
-    ],
 
     mouvements: [],
 
+
     achats: [],
+
 
     sessions: [],
 
+
     archives: [],
+
 
     clients: [],
 
+
     statistiques: {},
 
+
     parametres: {}
+
 
 };
 
 
 
-if (!db) {
 
-    db = structureDB;
+//--------------------------------------
+// Création première base
+//--------------------------------------
+
+if(!db){
+
+
+    db = {
+
+
+        commandes: [],
+
+
+        articles: [],
+
+
+        emplacements: [...emplacementsDefaut],
+
+
+        mouvements: [],
+
+
+        achats: [],
+
+
+        sessions: [],
+
+
+        archives: [],
+
+
+        clients: [],
+
+
+        statistiques: {},
+
+
+        parametres: {}
+
+
+    };
+
 
     sauvegarderDB();
 
+
 }
 
-else {
+else{
 
 
-    Object.keys(structureDB).forEach(function(cle){
+    // Vérification des anciennes versions
+
+    Object.keys(structureDB).forEach(cle=>{
+
 
         if(db[cle] === undefined){
 
-            db[cle] = structureDB[cle];
+
+            if(Array.isArray(structureDB[cle])){
+
+
+                db[cle] = [...structureDB[cle]];
+
+
+            }
+
+            else{
+
+
+                db[cle] = {};
+
+
+            }
+
 
         }
+
 
     });
 
 
+
+    // Sécurité supplémentaire stock
+
+
+    if(!Array.isArray(db.articles)){
+
+
+        db.articles=[];
+
+
+    }
+
+
+
+    if(!Array.isArray(db.mouvements)){
+
+
+        db.mouvements=[];
+
+
+    }
+
+
+
+    if(!Array.isArray(db.emplacements)
+       || db.emplacements.length===0){
+
+
+        db.emplacements=[...emplacementsDefaut];
+
+
+    }
+
+
+
     sauvegarderDB();
+
 
 }
 
 
 
-function sauvegarderDB() {
+
+//--------------------------------------
+// Sauvegarde centrale
+//--------------------------------------
+
+function sauvegarderDB(){
 
 
     localStorage.setItem(
@@ -93,8 +212,38 @@ function sauvegarderDB() {
 
 
 
+
+//--------------------------------------
+// Accès base complète
+//--------------------------------------
+
 function obtenirDB(){
 
     return db;
+
+}
+
+
+
+
+//--------------------------------------
+// Sécurité affichage HTML
+//--------------------------------------
+
+function securiserTexte(texte){
+
+
+    return String(texte)
+
+        .replace(/&/g,"&amp;")
+
+        .replace(/</g,"&lt;")
+
+        .replace(/>/g,"&gt;")
+
+        .replace(/"/g,"&quot;")
+
+        .replace(/'/g,"&#039;");
+
 
 }
